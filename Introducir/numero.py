dddf325f5d6bcc97d9d9a33b4e6b77dc3bdb0961
Dan1l0p <dalova2003@gmail.com>
2022-10-24 19:53:37 +0200

    numero.py

---
+++ Introducir/numero.py
@@ -0,0 +1,34 @@
+import sys
+
+
+MIN=0
+MAX=100
+
+
+def solicitar_introducir_numero(invite):
+    """
+    Esta función verifica que tenemos un número
+    """
+    while True:
+        
+        print(invite, end=": ")
+        datoIntroducido = input()
+
+        try:
+            datoIntroducido = int(datoIntroducido)
+        except:
+            print("Solo están autorizados los caracteres [0-9].", file=sys.stderr)
+        else:
+            
+            return datoIntroducido
+
+def solicitar_introducir_numero_extremo(invite, minimum=MIN, maximum=MAX):
+    
+    invite = "{} entre {} y {} incluídos".format(invite, minimum, maximum)
+    while True:
+        
+        datoIntroducido = solicitar_introducir_numero(invite)
+
+        if minimum <= datoIntroducido <= maximum:
+            
+            return datoIntroducido
