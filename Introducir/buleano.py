19286be208ec8f154a80f29673e24fab4d6de56c
Dan1l0p <dalova2003@gmail.com>
2022-10-24 19:40:58 +0200

    buleano.py

---
+++ Introducir/buleano.py
@@ -0,0 +1,14 @@
+SI = ("0", "si","y","yes","1")
+VERDADERO = ("v","verdadero","t","true","1")
+
+def solicitar_introducir_si_o_no(invite):
+    try:
+        return input(invite).lower() in SI
+    except:
+        return False
+
+def solicitar_introducir_verdadero_o_falso(invite):
+    try:
+        return input(invite).lower() in VERDADERO
+    except:
+        return False
